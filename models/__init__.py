6dfa1440a7d8b8bc7702ccd425d933e4da6c398c
Huangshuqi <1105374939@qq.com>
2024-05-22 21:42:39 +1000

    Upload ViT Models

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .build_models import vit_e_patch14_224_in21k, vit_Giant_patch14_224_in21k, vit_giant_patch14_224_in21k, \
+    vit_Giant_patch14_384_in21k, vit_e_patch14_384_in21k, vit_giant_patch14_384_in21k, \
+    vit_base_patch16_224, vit_base_patch16_224_in21k, vit_base_patch32_224_in21k, \
+    vit_base_patch32_224, vit_large_patch16_224, vit_large_patch16_224_in21k, vit_large_patch32_224_in21k, \
+    vit_huge_patch14_224_in21k, vit_e_patch14_588_in21k
